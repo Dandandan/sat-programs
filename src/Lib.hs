20fc1ed809bf4f273f5e60f5eec535859d4df398
Daniël Heres <dheres@bol.com>
2018-08-04 21:49:58 +0200

    Find simple +1 program

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,4 +8,4 @@ eval :: SWord8 -> (SWord8 -> SWord8)
 eval x y = ite (x .== 0) y (y + 1)
 
 findProgram :: IO SatResult
-findProgram = sat $ \x -> forAll_ $ \y -> (inter x) y .== y + 1
+findProgram = sat $ \x -> forAll_ $ \y -> (eval x) y .== y + 1

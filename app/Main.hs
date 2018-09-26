8f9c065890cc204407f16b35266b3bca6b612ab5
Daniël Heres <dheres@bol.com>
2018-09-26 07:59:01 +0200

    Find map programs with an example

--- app/Main.hs
+++ app/Main.hs
@@ -9,10 +9,14 @@ import Data.SBV.Control
 synthesize :: [Int] -> IO ()
 synthesize [] = error "Not found within limits"
 synthesize (x:xs) = do
+    print x
     r <- findProgram x
     case r of
-        SatResult (Satisfiable c m) -> print r
-        _ -> synthesize xs
+        SatResult (Satisfiable c m) -> do
+            print r
+        x -> do
+            print x
+            synthesize xs
 
 main :: IO ()
 main = do
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,10 @@
 {-# LANGUAGE OverloadedLists #-}
-module Lib
-    ( findProgram
-    ) where
+module Lib where
 
 import Data.SBV
 import Data.SBV.List as List
 import Data.SBV.List.Bounded
+import Data.SBV.Control
 
 add :: SBV Word8
 add = 0
@@ -13,19 +12,19 @@ add = 0
 sub :: SBV Word8
 sub = 1
 
-eval :: SWord8 -> SWord8 -> SWord8
-eval x y = 
-           ite (x .== add) (y + 1) $
-           ite (x .== sub) (y - 1)
-            y
+eval :: Int -> SWord8 -> SList Word8 -> SList Word8
+eval i x y = 
+           ite (x .== add) (bmap i (*2) y) $
+           ite (x .== sub) (bmap i (+1) y)
+           y
 
-maxLength = 10
-
-evalProg :: SList Word8 -> SWord8 -> Int -> SWord8
-evalProg prog y l = 
-    bfoldr l eval y prog
+evalProg :: SList Word8 -> SList Word8 -> Int -> Int -> SList Word8
+evalProg prog y l i = 
+    bfoldr l (eval i) y prog
 
 findProgram :: Int -> IO SatResult
-findProgram i = sat $ \prog -> forAll_ $ \y ->
-        List.length prog .== literal (toInteger i) &&&
-        evalProg prog y i .== y + 1
+findProgram i = sat $ do
+        prog <- free "prog"
+        constrain $ List.length prog .== literal (toInteger i)
+        return $
+            evalProg prog [0] i 1 .== [1]
